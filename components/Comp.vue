4d5690acc4814d8fcd9e7faf8cf0d79ed04e97a6
FelipeO16 <85635331+FelipeO16@users.noreply.github.com>
2024-01-26 00:53:04 -0300

    chore: themes update

---
+++ components/Comp.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="card flex justify-content-center flex-wrap gap-3">
+    <Button label="Primary" />
+    <Button label="Secondary" severity="secondary" />
+    <Button label="Success" severity="success" />
+    <Button label="Info" severity="info" />
+    <Button label="Warning" severity="warning" />
+    <Button label="Help" severity="help" />
+    <Button label="Danger" severity="danger" />
+    <Button label="Contrast" severity="contrast" />
+  </div>
+</template>
+
+<script setup></script>
